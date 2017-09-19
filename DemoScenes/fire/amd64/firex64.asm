587d0243f33a8082fa5570ab51147171759e5f7e
Toby Opferman <toby@opferman.com>
2017-09-18 23:24:25 -0700

    shorten fire demo

--- DemoScenes/fire/amd64/firex64.asm
+++ DemoScenes/fire/amd64/firex64.asm
@@ -357,7 +357,7 @@ FRAME_TEXT EQU <150>
                             ; ; Add more text
                             db 0
 
-                  FrameCountDown dd 2500
+                  FrameCountDown dd 1500
 .CODE
 
 ;*********************************************************
