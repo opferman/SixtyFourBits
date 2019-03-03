74cefd474a98df05141c9caf94a5ed8c00f09d37
Toby Opferman <toby@opferman.com>
2019-03-03 13:06:23 -0800

    Added displaying value of t

--- DemoScenes/FractalA/AMD64/fractala.asm
+++ DemoScenes/FractalA/AMD64/fractala.asm
@@ -21,12 +21,18 @@
 ;*********************************************************
 include demoscene.inc
 include dbuffer_public.inc
+include font_public.inc
+
 ;*********************************************************
 ; External WIN32/C Functions
 ;*********************************************************
 extern LocalAlloc:proc
 extern LocalFree:proc
 extern sqrt:proc
+extern cos:proc
+extern sin:proc
+extern tan:proc
+
 
 ;*********************************************************
 ; Structures
@@ -47,6 +53,68 @@ PIXEL_HISTORY ends
 EQUATION_PARAMS struct
    Param          mmword    ?
 EQUATION_PARAMS ends
+
+LOCAL_VARS struct
+    LocalVar1  dq ?
+    LocalVar2  dq ?
+LOCAL_VARS ends
+
+
+PARAMFRAME struct
+    Param1         dq ?
+    Param2         dq ?
+    Param3         dq ?
+    Param4         dq ?
+    Param5         dq ?
+    Param6         dq ?
+    Param7         dq ?
+    Param8         dq ?
+PARAMFRAME ends
+
+SAVEREGSFRAME struct
+    SaveRdi        dq ?
+    SaveRsi        dq ?
+    SaveRbx        dq ?
+    SaveR14        dq ?
+    SaveR15        dq ?
+    SaveR12        dq ?
+    SaveRbp        dq ?
+	Padding        dq ?
+
+    SaveXmm6       oword ? 
+    SaveXmm7       oword ? 
+    SaveXmm8       oword ? 
+    SaveXmm9       oword ? 
+    SaveXmm10      oword ? 
+    SaveXmm11      oword ? 
+    SaveR13        dq ?
+SAVEREGSFRAME ends
+
+FUNC_PARAMS struct
+    ReturnAddress  dq ?
+    Param1         dq ?
+    Param2         dq ?
+    Param3         dq ?
+    Param4         dq ?
+    Param5         dq ?
+    Param6         dq ?
+    Param7         dq ?
+FUNC_PARAMS ends
+
+
+
+FRACTAL_DEMO_STRUCTURE struct
+   ParameterFrame PARAMFRAME      <?>
+   SaveFrame      SAVEREGSFRAME   <?>
+   LocalVariables LOCAL_VARS      <?>
+FRACTAL_DEMO_STRUCTURE ends
+
+FRACTAL_DEMO_STRUCTURE_FUNC struct
+   ParameterFrame PARAMFRAME      <?>
+   SaveFrame      SAVEREGSFRAME   <?>
+   LocalVariables LOCAL_VARS      <?>
+   FuncParams     FUNC_PARAMS     <?>
+FRACTAL_DEMO_STRUCTURE_FUNC ends
 
 ;*********************************************************
 ; Public Declarations
@@ -93,6 +161,9 @@ EQU_PARAMETERS EQU <18>
    XandY          mmword ?
    XandT          mmword ?
    YandT          mmword ?
+   Ten            mmword 10.0
+   StringSize     mmword 100000.0
+   FormatString   db  "t = -3.00000", 0
    DoubleBuffer     dq ?
    PixelHistory   PIXEL_HISTORY EQU_ITTERATIONS DUP(<>)
    Parameters     EQUATION_PARAMS EQU_PARAMETERS DUP(<>)
@@ -619,7 +690,9 @@ NESTED_ENTRY FractalA_Demo, _TEXT$00
   XOR RDX, RDX
   MOV RCX, [DoubleBuffer]
   DEBUG_FUNCTION_CALL Dbuffer_UpdateScreen
-
+  
+  MOV RCX, RSI
+  DEBUG_FUNCTION_CALL FractalA_DisplayT
   ;
   ; Update the frame counter and determine if the demo is complete.
   ;
@@ -670,6 +743,287 @@ NESTED_ENTRY FractalA_Free, _TEXT$00
   ADD RSP, SIZE STD_FUNCTION_STACK_MIN
   RET
 NESTED_END FractalA_Free, _TEXT$00
+
+
+
+;*********************************************************
+;  FractalA_DisplayT
+;
+;        Parameters: Master Context
+;
+;       
+;
+;
+;*********************************************************  
+NESTED_ENTRY FractalA_DisplayT, _TEXT$00
+ alloc_stack(SIZEOF FRACTAL_DEMO_STRUCTURE)
+ save_reg rdi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRdi
+ save_reg rsi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRsi
+ save_reg rbx, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbx
+.ENDPROLOG 
+ DEBUG_RSP_CHECK_MACRO
+
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param5[RSP], 3
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param6[RSP], 0
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param7[RSP], 0
+   MOV R9, 4
+   MOV R8, 600
+   MOV RDX, OFFSET FormatString
+   MOV RSI, RCX
+   DEBUG_FUNCTION_CALL FractalA_PrintWord
+
+   MOV RDI, OFFSET FormatString
+   PXOR xmm0, xmm0
+   MOVSD xmm1, [t_Input]
+   MOV BYTE PTR [RDI + 4], ' '
+   UCOMISD xmm1, xmm0
+   JAE @PositiveFont
+   MOV BYTE PTR [RDI + 4], '-'
+   MOVSD xmm3, [NegOne]
+   MULSD xmm1, xmm3
+@PositiveFont:
+   MOVSD xmm2, [StringSize]
+   MULSD xmm1, xmm2
+   CVTSD2SI RAX, xmm1
+@Display:
+   CMP RAX, 1000000
+   JAE @T_IS_TOO_BIG
+   MOV R8, 11
+@OurLittleLoop:
+   MOV EBX, 10
+   XOR EDX, EDX
+   DIV EBX
+   ADD DL, '0'
+   MOV BYTE PTR [RDI + R8], DL
+   DEC R8
+   CMP R8, 6
+   JA @OurLittleLoop
+   ADD AL, '0'
+
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param5[RSP], 3
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param6[RSP], 0
+   MOV FRACTAL_DEMO_STRUCTURE.ParameterFrame.Param7[RSP], 0FFFFFFh
+   MOV R9, 4
+   MOV R8, 600
+   MOV RDX, OFFSET FormatString
+   MOV RCX, RSI
+   DEBUG_FUNCTION_CALL FractalA_PrintWord
+
+@T_IS_TOO_BIG:
+  MOV rdi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRdi[RSP]
+  MOV rsi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRsi[RSP]
+  MOV rbx, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbx[RSP]
+
+  ADD RSP, SIZE FRACTAL_DEMO_STRUCTURE
+  RET
+NESTED_END FractalA_DisplayT, _TEXT$00
+
+
+;*********************************************************
+;  FractalA_PrintWord
+;
+;        Parameters: Master Context, String, X, Y, Font Size, Radians, Color
+;
+;       
+;
+;
+;*********************************************************  
+NESTED_ENTRY FractalA_PrintWord, _TEXT$00
+ alloc_stack(SIZEOF FRACTAL_DEMO_STRUCTURE)
+ save_reg rdi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRdi
+ save_reg rsi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRsi
+ save_reg rbx, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbx
+ save_reg rbp, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbp
+ save_reg r14, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR14
+ save_reg r15, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR15
+ save_reg r12, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR12
+ save_reg r13, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR13
+ MOVAPS FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm6[RSP], xmm6
+ MOVAPS FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm7[RSP], xmm7
+ MOVAPS FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm8[RSP], xmm8
+ MOVAPS FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm9[RSP], xmm9
+.ENDPROLOG 
+ DEBUG_RSP_CHECK_MACRO
+  MOV RDI, RCX ; Master Context
+  MOV R15, RDX ; String
+  MOV R14, R8  ; X Location
+  MOV R12, R9  ; Y Location
+  MOV FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param3[RSP], R12
+
+@Plasma_PrintStringLoop:
+  ;
+  ; Get the Bit Font
+  ;
+  XOR RCX, RCX
+  MOV CL, [R15]
+  DEBUG_FUNCTION_CALL Font_GetBitFont
+  TEST RAX, RAX
+  JZ @ErrorOccured
+
+  MOV FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param1[RSP], RAX
+  MOV RSI, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param5[RSP]
+  MOV FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param2[RSP], 8
+
+
+@VerticleLines:
+       MOV BL, 80h
+       MOV R13, R14
+
+@HorizontalLines:
+           MOV RAX, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param1[RSP]
+           TEST BL, [RAX]
+           JZ @NoPixelToPlot 
+
+           MOV  FRACTAL_DEMO_STRUCTURE_FUNC.LocalVariables.LocalVar1[RSP], RBX
+
+           ;
+           ; Let's get the Font Size in R9
+           ;
+           MOV R9, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param5[RSP]
+		   
+
+@PlotRotatedPixel:
+              MOV  FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param4[RSP], R9
+
+			  MOV RAX, R14 ; X
+			  MOV  R8, R12  ; Y
+
+
+			  JMP @PlotPixel
+			  ;
+			  ; Rotate
+			  ;
+			  ;
+			  ; cos(r)*x - sin(r)*y
+			  ;
+			  CVTSI2SD xmm6, R14 ; X
+			  CVTSI2SD xmm7, R12 ; Y
+
+			  MOV RAX, MASTER_DEMO_STRUCT.ScreenHeight[RDI]
+			  SHR RAX, 1
+			  CVTSI2SD xmm0, RAX
+			  SUBSD xmm7, xmm0
+
+			  MOV RAX, MASTER_DEMO_STRUCT.ScreenWidth[RDI]
+			  SHR RAX, 1
+			  CVTSI2SD xmm0, RAX
+			  SUBSD xmm6, xmm0
+
+			  MOVSD xmm0, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param6[RSP]
+			  DEBUG_FUNCTION_CALL cos
+			  MULSD xmm0, xmm6
+			  MOVSD xmm9, xmm0
+
+			  MOVSD xmm0, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param6[RSP]
+			  DEBUG_FUNCTION_CALL Sin
+			  MULSD xmm0, xmm7
+			  SUBSD xmm9, xmm0
+
+			  ;
+			  ; (sin(r)*x + cos(r)*y)
+			  ;
+			  MOVSD xmm0, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param6[RSP]
+			  DEBUG_FUNCTION_CALL Sin
+			  MULSD xmm0, xmm6
+			  MOVSD xmm6, xmm9
+			  MOVSD xmm9, xmm0
+
+			  MOVSD xmm0, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param6[RSP]
+			  DEBUG_FUNCTION_CALL cos
+			  MULSD xmm0, xmm7
+			  ADDSD xmm0, xmm9
+			  MOVSD xmm7, xmm0
+
+			  MOV RAX, MASTER_DEMO_STRUCT.ScreenHeight[RDI]
+			  SHR RAX, 1
+			  CVTSI2SD xmm0, RAX
+			  ADDSD xmm7, xmm0
+
+			  MOV RAX, MASTER_DEMO_STRUCT.ScreenWidth[RDI]
+			  SHR RAX, 1
+			  CVTSI2SD xmm0, RAX
+			  ADDSD xmm6, xmm0
+
+			  CVTTSD2SI RAX, xmm6 ; X
+			  CVTTSD2SI R8, xmm7  ; Y
+
+@PlotPixel:
+
+			  CMP RAX, MASTER_DEMO_STRUCT.ScreenWidth[RDI]
+			  JAE @PixelOffScreen
+
+			  CMP R8, MASTER_DEMO_STRUCT.ScreenHeight[RDI]
+			  JAE @PixelOffScreen
+
+			  MOV RCX, R8
+			  IMUL RCX, MASTER_DEMO_STRUCT.ScreenWidth[RDI]
+			  SHL RAX, 2
+			  SHL RCX, 2
+			  ADD RCX, RAX
+			  ADD RCX, [DoubleBuffer]
+                          MOV RAX, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param7[RSP]
+			  MOV DWORD PTR [RCX], EAX
+
+@PixelOffScreen:
+			INC R14
+			MOV  RBX, FRACTAL_DEMO_STRUCTURE_FUNC.LocalVariables.LocalVar1[RSP]
+			MOV  R9, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param4[RSP]
+			DEC R9
+			JNZ @PlotRotatedPixel
+			JMP @DonePlottingPixel
+
+@NoPixelToPlot:
+        ADD R14, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param5[RSP]
+@DonePlottingPixel:
+    SHR BL, 1
+    TEST BL, BL
+    JNZ @HorizontalLines
+
+  MOV R14, R13
+  INC R12
+  DEC RSI
+  JNZ @VerticleLines
+  
+  MOV RSI, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param5[RSP]
+  INC FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param1[RSP]
+  DEC FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param2[RSP]
+  CMP FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param2[RSP], 0
+  JA @VerticleLines
+
+  MOV R12, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param3[RSP]
+  
+
+  INC R15
+
+  MOV RCX, FRACTAL_DEMO_STRUCTURE_FUNC.FuncParams.Param5[RSP]
+  SHL RCX, 3
+  ADD R14, RCX
+  ADD R14, 3
+ 
+  CMP BYTE PTR [R15], 0 
+  JNE @Plasma_PrintStringLoop
+
+
+  MOV EAX, 1
+@ErrorOccured:
+ MOVAPS xmm6,  FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm6[RSP]
+ MOVAPS xmm7,  FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm7[RSP]
+ MOVAPS xmm8,  FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm8[RSP]
+ MOVAPS xmm9,  FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveXmm9[RSP]
+  MOV rdi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRdi[RSP]
+  MOV rsi, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRsi[RSP]
+  MOV rbx, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbx[RSP]
+  MOV rbp, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveRbp[RSP]
+  MOV r14, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR14[RSP]
+  MOV r15, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR15[RSP]
+  MOV r12, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR12[RSP]
+  MOV r13, FRACTAL_DEMO_STRUCTURE.SaveFrame.SaveR13[RSP]
+  ADD RSP, SIZE FRACTAL_DEMO_STRUCTURE
+  RET
+NESTED_END FractalA_PrintWord, _TEXT$00
+
+
+
 
 
 END
